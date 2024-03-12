7f88e0fa3852e95be8cbf7a5380f91f6983eb30d
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-12 19:00:03 +0000

    TC

--- main.cpp
+++ main.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
-	int prev, current;
-	/* ****************************
-	// Complete this code.
-	*************************** */
+	int prev = 0, current = 0;
+	int i = 0;
+	do {
+		cin >> prev >> current;
+		if  (i >= 1 && prev < current)
+		{
+			break;
+		}
+		i++;
+ 		} while (prev > current);
+		std::cout << " " << endl;
+		exit (0);
 }
